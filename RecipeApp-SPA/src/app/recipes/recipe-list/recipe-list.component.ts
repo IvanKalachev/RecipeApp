f32cfb5b2f12f880d16eef2c2db9930db287ed66
Ivan Kalachev <ivan.gk@gmail.com>
2018-09-19 11:39:49 +0300

    Navigation using components databinding

--- RecipeApp-SPA/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeApp-SPA/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,17 +7,22 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
     new Recipe('A test recipe', 'this is simply a test', 'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads' +
       '/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg'),
-    new Recipe('A test recipe', 'this is simply a test', 'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads' +
+    new Recipe('A test recipe 2', 'this is simply a test', 'https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads' +
       '/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
